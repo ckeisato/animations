ebb73f60c05b14b180d7c214e2efee83d3324565
Kei Sato <ckeisato@gmail.com>
2020-06-21 21:45:17 -0700

    styled + interpolation change

--- oscilloscope/app.js
+++ oscilloscope/app.js
@@ -1,13 +1,11 @@
-// upload song as local file + ADD CREDIT INFO!!!!
 // create oscilloscope 
 // create service + google storage bucket with other songs????
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 
 const main = () => {
-  console.log("herroooo");
-
   var audio = new Audio('simon-more_dreamland.mp3');
   audio.autoplay = true;
+  audio.loop = true;
 
   var context = new AudioContext();
   var analyser = context.createAnalyser();
@@ -17,39 +15,40 @@ const main = () => {
 
   var frequencyData = new Uint8Array(360);
 
-  var svgHeight = '300';
-  var svgWidth = '1200';
-  var barPadding = '1';
+  var svgHeight = '90vh';
+  var svgWidth = '90vw';
 
   function createSvg(parent, height, width) {
     return d3.select(parent).append('svg').attr('height', height).attr('width', width);
   }
 
-  var svg = createSvg('body', svgHeight, svgWidth);
+  var svg = createSvg('#svg-container', svgHeight, svgWidth);
 
   var line = d3.svg.line.radial()
     .interpolate("linear-closed")
-    .radius(d => (d / 2) + 50)
+    .radius(d => (d / 2) + 100)
     .angle((d, i) => i * (Math.PI/180));
 
   var path = svg.append('path')
     .attr('d', line(frequencyData))
-    .attr('stroke', 'green')
-    .attr('stroke-width', 3)
-    .attr('fill', 'white')
-    .attr('class', 'line')
-    .attr('transform', 'translate(200, 180)');
+    .attr('stroke', 'white')
+    .attr('stroke-width', 2)
+    .attr('fill', 'transparent')
+    .attr('class', 'line');
 
 
   // Continuously loop and update chart with frequency data.
   const renderChart = () => {
-   requestAnimationFrame(renderChart);
-
-   // Copy frequency data to frequencyData array.
+    requestAnimationFrame(renderChart);
     analyser.getByteFrequencyData(frequencyData);
-    
     svg.select('.line').attr('d', line(frequencyData));
   }
+
+
+  document.getElementById('select-interpolation').addEventListener('change', event => {
+    console.log('hello', event.target.value);
+    line.interpolate(event.target.value);
+  });
 
   // Run the loop
   renderChart();
