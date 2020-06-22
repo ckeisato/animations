66333f7b3bc34aa505fb4cc1ebaf91974e8fa6a0
Kei Sato <ckeisato@gmail.com>
2020-06-21 20:51:56 -0700

    things are working

--- oscilloscope/app.js
+++ oscilloscope/app.js
@@ -3,7 +3,6 @@
 // create service + google storage bucket with other songs????
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 
-
 const main = () => {
   console.log("herroooo");
 
@@ -16,8 +15,7 @@ const main = () => {
   source.connect(analyser);
   analyser.connect(context.destination);
 
-
-  var frequencyData = new Uint8Array(200);
+  var frequencyData = new Uint8Array(360);
 
   var svgHeight = '300';
   var svgWidth = '1200';
@@ -29,42 +27,32 @@ const main = () => {
 
   var svg = createSvg('body', svgHeight, svgWidth);
 
-  var spiral = Array.from({ length: 20 }, (_, i) => [
-    ((Math.PI / 3) /20) * i, // angle (in radians)
-    2 * i // radius
-  ])
+  var line = d3.svg.line.radial()
+    .interpolate("linear-closed")
+    .radius(d => (d / 2) + 50)
+    .angle((d, i) => i * (Math.PI/180));
 
+  var path = svg.append('path')
+    .attr('d', line(frequencyData))
+    .attr('stroke', 'green')
+    .attr('stroke-width', 3)
+    .attr('fill', 'white')
+    .attr('class', 'line')
+    .attr('transform', 'translate(200, 180)');
 
-  console.log('spiral', spiral);
-
-
-  var lineRadial = d3.svg.line.radial();  
-  svg.append("path")
-    .attr("transform", "translate(200, 200)")
-    .attr("d", lineRadial(spiral))
-    .attr("fill", "none")
-    .attr("stroke", "black");
 
   // Continuously loop and update chart with frequency data.
-  function renderChart() {
+  const renderChart = () => {
    requestAnimationFrame(renderChart);
 
    // Copy frequency data to frequencyData array.
     analyser.getByteFrequencyData(frequencyData);
-    var test = spiral.map((coord, index) => {
-      return [coord[0], frequencyData[index] * 4]
-    });
-
-    // console.log('test', test);
     
-    svg.select('path').attr("d", lineRadial(test));
+    svg.select('.line').attr('d', line(frequencyData));
   }
 
   // Run the loop
-  // renderChart();
-
-
-
+  renderChart();
 }
 
 main();
