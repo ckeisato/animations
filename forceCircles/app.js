34349fd958aa65cfc2173b7b6059db04a2c9b445
Kei Sato <kei.sato@nordstrom.com>
2021-03-05 21:39:39 -0800

    update animations

---
+++ forceCircles/app.js
@@ -0,0 +1,108 @@
+// globals
+var force = null;
+var pointerCharge = -1000;
+var nodeCharge = -5
+var gravity = 0.05;
+
+// helpers
+// returns whether or not a node is colliding
+var collide = function(node) {
+  var r = node.radius + 16,
+    nx1 = node.x - r,
+    nx2 = node.x + r,
+    ny1 = node.y - r,
+    ny2 = node.y + r;
+  
+  return function(quad, x1, y1, x2, y2) {
+    if (quad.point && (quad.point !== node)) {
+      var x = node.x - quad.point.x,
+        y = node.y - quad.point.y,
+        l = Math.sqrt(x * x + y * y),
+        r = node.radius + quad.point.radius;
+      if (l < r) {
+        l = (l - r) / l * .5;
+        node.x -= x *= l;
+        node.y -= y *= l;
+        quad.point.x += x;
+        quad.point.y += y;
+      }
+    }
+    return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+  };
+}
+
+
+var updateForce = function() {
+  force.charge(function(d, i) {
+    return i == 0 ? pointerCharge : nodeCharge;
+  });
+  force.start();
+}
+
+// UI functions
+var updatePointerCharge = function(targetID, val) {
+  document.getElementById(targetID).innerHTML = val;
+  pointerCharge = val;
+  updateForce();
+}
+
+var updateNodeCharge = function(targetID, val) {
+  document.getElementById(targetID).innerHTML = val;
+  nodeCharge = val;
+  updateForce();
+}
+
+var updateGravity = function(targetID, val) {
+  document.getElementById(targetID).innerHTML = val;
+  gravity = val;
+  force.gravity(gravity);
+  force.start();
+}
+
+var init = function() {
+  var width = 960, height = 500;
+  var nodes = d3.range(200).map(function() { return {radius: Math.random() * 12 + 4}; });
+  var root = nodes[0];
+  var colors = ["#9B5DE5", "#F15BB5", "#FEE440", "#00BBF9", "#00F5D4"];
+
+  root.radius = 0;
+  root.fixed = true;
+
+  force = d3.layout.force()
+    .gravity(gravity)
+    .charge(function(d, i) {
+      return i == 0 ? pointerCharge : nodeCharge;
+    })
+    .nodes(nodes)
+    .size([width, height]);
+
+  force.start();
+
+  var svg = d3.select("#container").append("svg");
+  svg.selectAll("circle")
+    .data(nodes.slice(1))
+    .enter().append("circle")
+    .attr("r", function(d) { return d.radius; })
+    .style("fill", function(d, i) { return colors[i%5] });
+
+  force.on("tick", function(e) {
+    var q = d3.geom.quadtree(nodes);
+    var i = 0;
+    var n = nodes.length;
+
+    while (++i < n) q.visit(collide(nodes[i]));
+
+    svg.selectAll("circle")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+  });
+
+  svg.on("mousemove", function() {
+    var p1 = d3.mouse(this);
+    root.px = p1[0];
+    root.py = p1[1];
+    force.resume();
+  });
+}
+
+init();
---
+++ oscilloscope/app.js
@@ -0,0 +1,61 @@
+const main = () => {
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  var audio = new Audio('ikson-late.mp3');
+  audio.loop = true;
+
+  var context = new AudioContext();
+  var analyser = context.createAnalyser();
+  var source = context.createMediaElementSource(audio);
+  source.connect(analyser);
+  analyser.connect(context.destination);
+
+  var frequencyData = new Uint8Array(360);
+
+  var svg = d3.select('#svg-container').append('svg').attr('height', '85vh').attr('width', '90vw');
+
+  var line = d3.svg.line.radial()
+    .interpolate("linear-closed")
+    .radius(d => (d / 2) + 150)
+    .angle((d, i) => i * (Math.PI/180));
+
+  var path = svg.append('path')
+    .attr('d', line(frequencyData))
+    .attr('stroke', 'white')
+    .attr('stroke-width', 2)
+    .attr('fill', 'transparent')
+    .attr('class', 'line');
+
+  // Continuously loop and update chart with frequency data.
+  const renderChart = () => {
+    requestAnimationFrame(renderChart);
+    analyser.getByteFrequencyData(frequencyData);
+    svg.select('.line').attr('d', line(frequencyData));
+  }
+
+
+  document.getElementById('select-interpolation').addEventListener('change', event => {
+    line.interpolate(event.target.value);
+  });
+
+  document.getElementById('audio-cta').addEventListener('click', event => {
+    context.resume();
+    if (audio.paused) {
+      event.target.innerText = "Pause"
+      audio.play();
+    } else {
+      audio.pause();
+      event.target.innerText = "Play"
+    }
+  });
+
+  setInterval(() => {
+    document.getElementById('container').classList.toggle('gradient-two')
+  }, 10000);
+
+  // Run the loop
+  renderChart();
+}
+
+window.onload = () => {
+  main();
+}
